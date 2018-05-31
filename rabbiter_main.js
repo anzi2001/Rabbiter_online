f2a948e9bcb8c72e4bfaf0444689b42963258461
anzi2001 <kocjancic.anze@gmail.com>
2018-05-31 21:00:55 +0200

    initialized db connection

--- rabbiter_main.js
+++ rabbiter_main.js
@@ -1,6 +1,18 @@
 var http = require("http").createServer(listener);
 var io = require("socket.io")(http);
+var mysql= require("mysql");
 http.listen(8080);
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: ""
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("DB WORKS");
+});
 
 function listener(req,res){
     req.writeHead(200,{"content type":"text/html"});
@@ -8,4 +20,4 @@ function listener(req,res){
 }
 io.on("connected",function(){
     console.log("user connected");
-})
+});
